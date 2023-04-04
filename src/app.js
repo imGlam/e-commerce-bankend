c4eaba2f7547119681901ac301228a48d44a69a4
imGlam <gialamhuynh3004@gmail.com>
2023-04-04 21:08:10 +0700

    section 2 - necessary folders

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+// init middlewares
+app.use(morgan('dev'))
+app.use(helmet())
+// app.use(compression())
+
+// init db
+
+// init routers
+
+// handling errors
+
+module.exports = app
